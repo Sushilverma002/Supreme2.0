17f17498a1bec5e24e5c60e8e528ff653bdf76e3
Sushil Verma <114687502+Sushilverma002@users.noreply.github.com>
2023-09-10 11:41:58 +0530

    push

---
+++ BasicsOfPrograming/Array2.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+// printing arrays
+void printArray(int arr[], int n)
+{
+    arr[0] = 100;
+}
+int getunique(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                cout << arr[i] << "," << arr[j] << " " << arr[k] << endl;
+            }
+        }
+    }
+}
+
+// shift array
+void shiftarray(int arr[], int n)
+{
+    // step 1
+    int temp = arr[n - 1];
+    for (int i = n - 1; i >= 1; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+}
+
+// count 0 and 1 approach 1;
+void countZeroOne(int arr[], int n)
+{
+    int zeroCount = 0;
+    int oneCount = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+        {
+            zeroCount++;
+        }
+        if (arr[i] == 1)
+        {
+            oneCount++;
+        }
+    }
+    // // step 2 print
+    // int i;
+    // for (i = 0; i < zeroCount; i++)
+    // {
+    //     arr[i] = 0;
+    // }
+    // for (int j = i; j < n; j++)
+    // {
+    //     arr[j] = 1;
+    // }
+    int index = 0;
+    while (zeroCount--)
+    {
+        arr[index] = 0;
+        index++;
+    }
+    while (oneCount--)
+    {
+        arr[index] = 1;
+        index++;
+    }
+}
+
+// approach 2
+void CountZeroOneTwoPointer(int arr[], int n)
+{
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        while (arr[i] == 0 && i < j)
+        {
+            i++;
+        }
+        while (arr[j] == 1 && i < j)
+        {
+            j--;
+        }
+        while (arr[i] == 1 && arr[j] == 0)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+}
+int main()
+{
+    // int n = 5;
+    // int arr[5] = {5, 4, 6, 2, 3};
+    // printArray(arr, n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // int n = 6;
+    // int arr[] = {1, 3, 2, 5, 4, 6};
+    // int ans = getunique(arr, n);
+    // cout << ans;
+
+    // int n = 5;
+    // int arr[] = {1, 2, 3, 4, 5};
+    // shiftarray(arr, n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << ' ';
+    // }
+
+    // sort zero and one
+    int n = 10;
+    int arr[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+    // countZeroOne(arr, n);
+    CountZeroOneTwoPointer(arr, 10);
+    // print
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
--- BasicsOfPrograming/tempCodeRunnerFile.cpp
+++ BasicsOfPrograming/tempCodeRunnerFile.cpp
@@ -1,10 +1,1 @@
-for (int i = n; i >= 1; i--)
-    // {
-    //     for (int j = 1; j <= i; j++)
-    //         cout << "*";
-    //     for (int j = 1; j <= 2 * (n - i); j++)
-    //         cout << " ";
-    //     for (int j = 1; j <= i; j++)
-    //         cout << "*";
-    //     cout << "\n";
-    // }
+    // countZeroOne(arr, n);
